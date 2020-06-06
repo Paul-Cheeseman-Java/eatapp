133c2edff6b21f02400d2abe6c63ee66f48fce94
Paul Cheeseman <Paul_Cheeseman@zoho.com>
2020-06-06 09:47:35 +0100

    basic app start with db connection

---
+++ src/main/java/com/demo/eatapp/EatappApplication.java
@@ -0,0 +1,13 @@
+package com.demo.eatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/eatapp/EatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.eatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
